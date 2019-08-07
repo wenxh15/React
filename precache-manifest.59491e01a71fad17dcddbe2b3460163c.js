3962ed8baee9b12f96271177fed9de7efb1bc0d9
Wenxuan He <wenxh15@vt.edu>
2019-08-06 18:53:06 -0700

    Updates

---
+++ precache-manifest.59491e01a71fad17dcddbe2b3460163c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df36f6f3d2baf0c6024aa20cfa30d5f",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "ab6ab6c50eb5ff90962d",
+    "url": "/React/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/React/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ab6ab6c50eb5ff90962d",
+    "url": "/React/static/js/main.7b1cdf51.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React/static/media/logo.5d5d9eef.svg"
+  }
+]);
